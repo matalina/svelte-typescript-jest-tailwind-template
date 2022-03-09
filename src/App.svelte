5af76a8d199d0836787c7e9a3b31793a0c68b2c1
Alicia Wilkerson <matalina@gmail.com>
2022-03-09 08:08:15 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Tailwind from "./components/Tailwind.svelte";
+
+	export let name: string;
+</script>
+
+<Tailwind />
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const production = !process.env.ROLLUP_WATCH;
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    content: [
+      "./src/**/*.svelte",
+    ],
+    enabled: production // disable purge in dev
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors,
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  }
+}
